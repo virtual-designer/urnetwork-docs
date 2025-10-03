2b4bc87f7a163220ee5d7477a3da0ac5c1bd5213
Ar Rakin <rakinar2@osndevs.org>
2025-10-03 16:25:35 +0600

    fix: edit button options
    Signed-off-by: Ar Rakin <rakinar2@osndevs.org>

--- components/MDX/EditButton.tsx
+++ components/MDX/EditButton.tsx
@@ -2,18 +2,19 @@
 
 import { GITHUB_REPOSITORY, GITHUB_REPOSITORY_BRANCH } from "@/config/config";
 import {
-    Button,
-    ButtonGroup,
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownTrigger,
+	Button,
+	ButtonGroup,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownTrigger,
 } from "@heroui/react";
+import { Key } from "react";
 import { BsGit } from "react-icons/bs";
 import {
-    HiChevronDown,
-    HiOutlineClipboardDocument,
-    HiOutlineLink,
+	HiChevronDown,
+	HiOutlineClipboardDocument,
+	HiOutlineLink,
 } from "react-icons/hi2";
 import { MdEdit } from "react-icons/md";
 import Link from "../Navigation/Link";
@@ -35,6 +36,7 @@ export default function EditButton({ editPath, isChanging }: EditButtonProps) {
 		  )}/${editPath ?? ""}`
 		: "#";
 	const isDisabled = !editPath || isChanging;
+
 	const copyPageContentsPlain = async () => {
 		const article = document.getElementById("article");
 
@@ -48,11 +50,25 @@ export default function EditButton({ editPath, isChanging }: EditButtonProps) {
 			console.error(error);
 		}
 	};
+
 	const copyURL = async () => {
 		try {
-			await navigator.clipboard.writeText(location.href);
+			const url = location.toString();
+			await navigator.clipboard.writeText(url);
 		} catch (error) {
 			console.error(error);
+		}
+	};
+
+	const onAction = (key: Key) => {
+		switch (key) {
+			case "copyPlain":
+				copyPageContentsPlain();
+				break;
+
+			case "copyURL":
+				copyURL();
+				break;
 		}
 	};
 
@@ -67,7 +83,10 @@ export default function EditButton({ editPath, isChanging }: EditButtonProps) {
 			>
 				Edit this page
 			</Button>
-			<Dropdown placement="bottom-end" className="editbuttonOptionsDropdown">
+			<Dropdown
+				placement="bottom-end"
+				className="editbuttonOptionsDropdown"
+			>
 				<DropdownTrigger>
 					<Button isIconOnly isDisabled={isDisabled}>
 						<HiChevronDown />
@@ -78,12 +97,13 @@ export default function EditButton({ editPath, isChanging }: EditButtonProps) {
 					aria-label="Options"
 					className="max-w-[300px] editbuttonOptions"
 					selectionMode="single"
+					onAction={onAction}
+					variant="flat"
 				>
 					<DropdownItem
 						key="copyPlain"
 						description="Copy the page contents as plain text."
 						startContent={<HiOutlineClipboardDocument size={30} />}
-						onPress={copyPageContentsPlain}
 					>
 						Copy page contents
 					</DropdownItem>
@@ -91,7 +111,6 @@ export default function EditButton({ editPath, isChanging }: EditButtonProps) {
 						key="copyURL"
 						description="Copy the link of this page."
 						startContent={<HiOutlineLink size={30} />}
-						onPress={copyURL}
 					>
 						Copy URL
 					</DropdownItem>
